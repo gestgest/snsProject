8dda79a25c017bcc1c8f34a7466ef3a62c1c835a
brekJ <118181118+hs-1891304-jaehoonjeong@users.noreply.github.com>
2022-11-20 19:16:04 +0900

    UploadPoasterScreen edited

--- lib/Screen/HomeScreen.dart
+++ lib/Screen/HomeScreen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
   }
-
 }
