861d3c17d2ceacbad4d0a87c5dbf0ad154cb7d0e
gestgest <87427692+gestgest@users.noreply.github.com>
2022-11-20 19:00:55 +0900

    메인 + 마이페이지

---
+++ lib/Widget/BottomBar.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Container(
+        height: 60,
+        width: 300,
+        child: TabBar(
+          tabs: <Widget>[
+            Tab(
+                icon: Icon(
+                  Icons.home,
+                  size: 23,
+                ),
+                child: Text('홈', style: TextStyle(fontSize: 9))),
+            Tab(
+                icon: Icon(
+                  Icons.add_circle,
+                  size: 23,
+                ),
+                child: Text('게시물 등록', style: TextStyle(fontSize: 9))),
+            Tab(
+                icon: Icon(
+                  Icons.account_circle,
+                  size: 23,
+                ),
+                child: Text('마이페이지', style: TextStyle(fontSize: 9))),
+          ],
+          labelColor: Color(0xFF957DAD),
+          unselectedLabelColor:
+          Theme.of(context).highlightColor.withOpacity(0.8),
+          indicatorColor: Colors.transparent,
+        ),
+      ),
+    );
+  }
+
+}
