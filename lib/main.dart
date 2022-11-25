b386d3a99ea519593dbf7ef056e08997bfe6be7a
gestgest <87427692+gestgest@users.noreply.github.com>
2022-11-26 01:09:53 +0900

    메인 게시물 가져오기 기능 추가 + 게시물 서비스

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:snsproject/Screen/UploadPosterScreen.dart';
 import 'Screen/HomeScreen.dart';
 import 'Screen/MyPageScreen.dart';
+import 'package:provider/provider.dart';
+import 'Service/PosterService.dart';
 import 'Widget/BottomBar.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => PosterService()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
