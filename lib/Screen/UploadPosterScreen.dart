72b420af6a825b34bdf45e3bcbfc5b9b68431ecd
brekJ <118181118+hs-1891304-jaehoonjeong@users.noreply.github.com>
2022-11-25 22:11:43 +0900

    uploadScreen update

--- lib/Screen/UploadPosterScreen.dart
+++ lib/Screen/UploadPosterScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'custom_text_form_field.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -9,9 +10,9 @@ import 'package:image_picker/image_picker.dart';
 import 'package:snsproject/Screen/HomeScreen.dart';
 import 'package:snsproject/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:multi_image_picker/multi_image_picker.dart';
 import 'package:dotted_border/dotted_border.dart';
-import 'package:cupertino_icons/cupertino_icons.dart';
+import 'package:dio/dio.dart';
+import 'package:http_parser/http_parser.dart';
 //final Future<XFile?> image = _picker.pickImage(source: ImageSource.gallery);
 //final Future<List<XFile>?> images = _picker.pickMultiImage();
 
@@ -21,6 +22,11 @@ class UploadPosterScreen extends StatefulWidget {
 }
 
 class _UploadPosterScreenState extends State<UploadPosterScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   final ImagePicker _picker = ImagePicker();
   List<XFile> _pickedImgs = [];
 
@@ -33,40 +39,72 @@ class _UploadPosterScreenState extends State<UploadPosterScreen> {
     }
   }
 
-  List<Widget> _boxContents = [
-    IconButton(
-        onPressed: () {
-          _pickImg();
-        },
-        icon: Container(
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.6), shape: BoxShape.circle),
-            child: Icon(
-              Icons.camera_alt,
-              color: Colors.transparent,
-            ))),
-    Container(),
-    Container(),
-    _pickedImgs.length <= 4
-        ? Container()
-        : FittedBox(
-            child: Container(
-                padding: EdgeInsets.all(6),
-                decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.6),
-                    shape: BoxShape.circle),
-                child: Text(
-                  '+${(_pickedImgs.length - 4).toString()}',
-                  style: Theme.of(context)
-                      .textTheme
-                      .subtitle2
-                      ?.copyWith(fontWeight: FontWeight.w800),
-                ))),
-  ];
+  /*uploadImage() async {
+    print(_pickedImgs.toString());
+    var dio = Dio();
+    try {
+      List<MultipartFile> multipartImgList = [];
+
+      for (int i = 0; i < _pickedImgs.length; i++) {
+        var pic = await MultipartFile.fromFile(_pickedImgs[i].path,
+            contentType: new MediaType("image", "jpg"));
+        multipartImgList.add(pic);
+      }
+
+      FormData formData = new FormData.fromMap({
+        "images": multipartImgList,
+      });
+
+      dio.options.headers["authorization"] = context.read<AuthProvider>().token.toString();
+      
+      dio
+          .post(
+            uploadHospVideoUrl.toString(),
+            data: formData,
+          )
+          .then((value) => print(value));
+      print(context.read<AuthProvider>().token.toString());
+    } catch (e) {
+      print(e);
+    }
+  };*/
 
   @override
   Widget build(BuildContext context) {
+    bool isPadMode = MediaQuery.of(context).size.width > 700;
+
+    List<Widget> _boxContents = [
+      IconButton(
+          onPressed: () {
+            _pickImg();
+          },
+          icon: Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.6), shape: BoxShape.circle),
+              child: Icon(
+                Icons.camera_alt,
+                color: Theme.of(context).colorScheme.primary,
+              ))),
+      Container(),
+      Container(),
+      _pickedImgs.length <= 4
+          ? Container()
+          : FittedBox(
+              child: Container(
+                  padding: EdgeInsets.all(6),
+                  decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.6),
+                      shape: BoxShape.circle),
+                  child: Text(
+                    '+${(_pickedImgs.length - 4).toString()}',
+                    style: Theme.of(context)
+                        .textTheme
+                        .subtitle2
+                        ?.copyWith(fontWeight: FontWeight.w800),
+                  ))),
+    ];
+
     return Scaffold(
         appBar: AppBar(
             title: const Text("새 게시물"),
@@ -90,9 +128,11 @@ class _UploadPosterScreenState extends State<UploadPosterScreen> {
               mainAxisSpacing: 5,
               crossAxisSpacing: 5,
               children: List.generate(
-                  4,
+                  _pickedImgs.length,
                   (index) => DottedBorder(
-                      child: Container(),
+                      child: Container(
+                        child: Center(child: _boxContents[index]),
+                      ),
                       color: Colors.grey,
                       dashPattern: [5, 3],
                       borderType: BorderType.RRect,
