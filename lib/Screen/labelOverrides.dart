ecf874b66646ca04b9087521998121682d0eb3a9
1891318 최동규 <ehdrb924@kakao.com>
2022-11-27 23:14:08 +0900

    로그인 + 회원가입
    입니다

---
+++ lib/Screen/labelOverrides.dart
@@ -0,0 +1,48 @@
+import 'package:flutterfire_ui/i10n.dart';
+
+class LabelOverrides extends DefaultLocalizations {
+  const LabelOverrides();
+
+  @override
+  String get emailInputLabel => '이메일 입력';
+
+  @override
+  String get passwordInputLabel => '비밀번호 입력';
+
+  @override
+  String get signInText => '로그인';
+
+  @override
+  String get registerHintText => '계정이 없으신가요? ';
+
+  @override
+  String get registerText => '회원 가입';
+
+  @override
+  String get confirmPasswordInputLabel => '비밀번호 확인';
+
+  @override
+  String get forgotPasswordButtonLabel => '비밀번호 찾기';
+
+  @override
+  String get forgotPasswordHintText => '사용하셨던 이메일을 입력해주세요!';
+
+  @override
+  String get forgotPasswordViewTitle => '비밀번호를 잊어버리셨나요?';
+
+  @override
+  String get resetPasswordButtonLabel => '비밀번호 초기화';
+
+  @override
+  String get signInButtonText => '로그인';
+
+  @override
+  String get signInHintText => '계정이 존재하시나요? ';
+
+  @override
+  String get signInActionText => '로그인';
+
+  @override
+  String get registerActionText => '회원 가입';
+}
+
